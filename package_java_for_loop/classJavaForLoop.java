dd63c53abab6ffb78366dab1394fe0188d3e387a
Daniel Urra <danielurra75@gmail.com>
2024-01-31 07:27:26 -0500

    final version

--- package_java_for_loop/classJavaForLoop.java
+++ package_java_for_loop/classJavaForLoop.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class classJavaForLoop {
     public static void main(String[] args) {
         Scanner dannyScan = new Scanner(System.in);
+        int[] eachNumber = new int[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Please enter one number: ");
+            int numberFromScanner = Integer.parseInt(dannyScan.nextLine());
+            eachNumber[i] = numberFromScanner;
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Number " + i + ": " + eachNumber[i]);
+        }
 
     }
 }
