93cfa9a5dfeadf61486d9a891bc000c9b0190958
Daniel Urra <danielurra75@gmail.com>
2024-01-31 06:52:48 -0500

    first commit

---
+++ package_java_for_loop/classJavaForLoop.java
@@ -0,0 +1,10 @@
+package package_java_for_loop;
+
+import java.util.Scanner;
+
+public class classJavaForLoop {
+    public static void main(String[] args) {
+        Scanner dannyScan = new Scanner(System.in);
+
+    }
+}
